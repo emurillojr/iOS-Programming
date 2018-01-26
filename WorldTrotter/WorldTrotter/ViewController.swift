f721ba84192491a597ede093aa619c1d57980991
user135340 <user135340@NY166.local>
2018-01-25 21:19:34 -0500

    WorldTrotter 3

--- WorldTrotter/WorldTrotter/ViewController.swift
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -9,24 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // created instance of UIView frame with background color of blue
-        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
-        let firstView = UIView(frame: firstFrame)
-        firstView.backgroundColor = UIColor.blue
-        view.addSubview(firstView)
-        
-        // created another instance of UIView frame with background color of green
-        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
-        let secondView = UIView(frame: secondFrame)
-        secondView.backgroundColor = UIColor.green
-        //view.addSubview(secondView)
-        // adjust view hierarchy
-        firstView.addSubview(secondView)
-    }
-
+    
 
 }
 
