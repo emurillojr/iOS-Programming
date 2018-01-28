cc81facf815a6ca4c7014f67ee53f266c5c83384
user135340 <user135340@NY166.local>
2018-01-28 11:54:11 -0500

    WorldTrotter 4 p81

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -9,24 +9,72 @@
 import UIKit
 
 class ConversionViewController : UIViewController {
-    
     // create an outlet to the Celsius text label and create an action for the text field to call when the text changes
     @IBOutlet var celsiusLabel: UILabel!
+    // store the current Fahrenheit value, optional measurement for temperature
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        // Add a property observer to fahrenheitValue that gets called after the property value changes.
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    // add a computed property for the Celsius value. This value will be computed based on the Fahrenheit value.
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil }
+    }
+    
     @IBOutlet var textField: UITextField! // outlet for background view is tapped
+    
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
         //celsiusLabel.text = textField.text
         // to display “???” if the text field is empty.
-        if let text = textField.text, !text.isEmpty {
-            celsiusLabel.text = text
+        //if let text = textField.text, !text.isEmpty {
+        //    celsiusLabel.text = text
+        //} else {
+        //    celsiusLabel.text = "???"
+        //}
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
         } else {
-            celsiusLabel.text = "???"
+            fahrenheitValue = nil
         }
-        
     }
     
     @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
         textField.resignFirstResponder()
     }
+    
+    // updates the celsius label
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            //celsiusLabel.text = "\(celsiusValue.value)"
+            // modified updateCelsiusLabel() to use this formatter
+            celsiusLabel.text =
+                numberFormatter.string(from: NSNumber(value: celsiusValue.value))        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    // update celsiusLabel when application runs insead of showing 100
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateCelsiusLabel()
+    }
+    
+    // Created a constant number formatter
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
     
     
     
