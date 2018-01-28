8704366824466d168a00ab91b410d32f1b8b28bb
user135340 <user135340@NY166.local>
2018-01-28 11:40:29 -0500

    WorldTrotter 4.11

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -12,11 +12,22 @@ class ConversionViewController : UIViewController {
     
     // create an outlet to the Celsius text label and create an action for the text field to call when the text changes
     @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField! // outlet for background view is tapped
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
-        celsiusLabel.text = textField.text
-        
+        //celsiusLabel.text = textField.text
+        // to display “???” if the text field is empty.
+        if let text = textField.text, !text.isEmpty {
+            celsiusLabel.text = text
+        } else {
+            celsiusLabel.text = "???"
+        }
         
     }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
     
     
 }
