28d57e21ea64189bd00f3a1972f52936c1308dae
user135340 <user135340@NY166.local>
2018-02-03 14:18:58 -0500

    ch. 11 done

--- Homepwner/Homepwner/ItemStore.swift
+++ Homepwner/Homepwner/ItemStore.swift
@@ -20,13 +20,31 @@ class ItemStore {
     }
     
     // implement the designated initializer to add five random items
-    init() {
-        for _ in 0..<5 {
-            createItem()
+    //init() {
+    //    for _ in 0..<5 {
+    //        createItem()
+    //    }
+    //}
+    
+    
+    // implement a new method to remove a specific item
+    func removeItem(_ item: Item) {
+        if let index = allItems.index(of: item) {
+            allItems.remove(at: index)
         }
     }
     
-    
+    // implement a new method for moving rows
+    func moveItem(from fromIndex: Int, to toIndex: Int) {
+        if fromIndex == toIndex {
+            return }
+        // Get reference to object being moved so you can reinsert it
+        let movedItem = allItems[fromIndex]
+        // Remove item from array
+        allItems.remove(at: fromIndex)
+        // Insert item in array at new location
+        allItems.insert(movedItem, at: toIndex)
+    }
 }
 
 
--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -13,6 +13,45 @@ class ItemsViewController: UITableViewController {  // implement a subclass of U
     // add a property for an ItemStore
     var itemStore: ItemStore!
     
+    // stub out two methods in the implementation
+    @IBAction func addNewItem(_ sender: UIButton) {
+        // implement addNewItem(_:)
+        
+        // Make a new index path for the 0th section, last row
+        //let lastRow = tableView.numberOfRows(inSection: 0)
+        //let indexPath = IndexPath(row: lastRow, section: 0)
+        // Insert this new row into the table
+        //tableView.insertRows(at: [indexPath], with: .automatic)
+        
+        // Create a new item and add it to the store
+        let newItem = itemStore.createItem()
+        // Figure out where that item is in the array
+        if let index = itemStore.allItems.index(of: newItem) {
+            let indexPath = IndexPath(row: index, section: 0)
+            // Insert this new row into the table
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+        
+        
+    }
+    @IBAction func toggleEditingMode(_ sender: UIButton) {
+      // implement toggleEditingMode(_:)
+     
+        // If you are currently in editing mode...
+        if isEditing {
+            // Change text of button to inform user of state
+            sender.setTitle("Edit", for: .normal)
+            // Turn off editing mode
+            setEditing(false, animated: true)
+        } else {
+            // Change text of button to inform user of state
+            sender.setTitle("Done", for: .normal)
+            // Enter editing mode
+            setEditing(true, animated: true)
+        }
+    
+    }
+    
     // implement tableView(_:numberOfRowsInSection:).
     override func tableView(_ tableView: UITableView,
                             numberOfRowsInSection section: Int) -> Int {
@@ -25,7 +64,7 @@ class ItemsViewController: UITableViewController {  // implement a subclass of U
         // Create an instance of UITableViewCell, with default appearance
         //let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
         
-        // updated to update tableView(_:cellForRowAt:) to reuse cells.
+        // updated to update tableView(_:cellForRowAt:) to reuse cells
         // Get a new or recycled cell
         let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell",
                                                  for: indexPath)
@@ -39,6 +78,46 @@ class ItemsViewController: UITableViewController {  // implement a subclass of U
         return cell
         
     }
+    
+    // implement this method to have the ItemStore remove the right object and confirm the row deletion by calling the method deleteRows(at:with:) on the table view
+    override func tableView(_ tableView: UITableView,
+                            commit editingStyle: UITableViewCellEditingStyle,
+                            forRowAt indexPath: IndexPath) {
+        // If the table view is asking to commit a delete command...
+        if editingStyle == .delete {
+            let item = itemStore.allItems[indexPath.row]
+            
+            // to ask the user to confirm or cancel the deletion of an item
+            let title = "Delete \(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+            let ac = UIAlertController(title: title,
+                                       message: message,
+                                       preferredStyle: .actionSheet)
+            
+            // Add the necessary actions to the action sheet in tableView(_:commit:forRowAt:)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            ac.addAction(cancelAction)
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive,
+                                             handler: { (action) -> Void in
+            // Remove the item from the store
+            self.itemStore.removeItem(item)
+            // Also remove that row from the table view with an animation
+            self.tableView.deleteRows(at: [indexPath], with: .automatic) })
+            ac.addAction(deleteAction)
+            // Present the alert controller
+            present(ac, animated: true, completion: nil)
+            
+        }
+    }
+    
+    // implement tableView(_:moveRowAt:to:) to update the store
+    override func tableView(_ tableView: UITableView,
+                            moveRowAt sourceIndexPath: IndexPath,
+                            // Update the model
+        to destinationIndexPath: IndexPath) {
+        itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+    }
+    
     
     // override viewDidLoad() to update the table view content inset
     override func viewDidLoad() {
