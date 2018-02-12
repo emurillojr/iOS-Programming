f34730b616f23893c54060a7df91de068bba735f
user135340 <user135340@NY166.local>
2018-02-12 12:34:21 -0500

    updated

--- WeatherApp/WeatherApp/API/APIClient.swift
+++ WeatherApp/WeatherApp/API/APIClient.swift
@@ -6,14 +6,18 @@
 //  Copyright © 2018 Murillo. All rights reserved.
 //
 
+// responsibile for creating a request to the weather api
+
 import Foundation
 
-enum Either<V, E: Error> {
-    case value(V)
-    case error(E)
+// object
+enum Either<V, E: Error> { // conforms to Error protocol
+    case value(V) // pass in value
+    case error(E) // pass in error
 }
 
-enum APIError: Error {
+// object
+enum APIError: Error { // conforms to Error protocol
     case apiError
     case badResponse
     case jsonDecoder
@@ -22,13 +26,38 @@ enum APIError: Error {
 
 // protocol
 protocol APIClient {
-    // responsibile for creating a request to the weather api
+    
     var session: URLSession { get }
     
+    // generic function constrained to any type that is Codable
+    // V for value, completion handler, @escaping - when the closure is passed as an argument to the function, but is called after the function returns, closer captures result type either error from api or weather object from api
     func fetch<V: Codable>(with request: URLRequest, completion: @escaping (Either<V, APIError>) -> Void)
 }
 
 // protocol extension
 extension APIClient {
-    
+    func fetch<V: Codable>(with request: URLRequest, completion: @escaping (Either<V, APIError>) -> Void){
+        // provide default implementation
+        // create task since will be making request to api
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // if we have an error
+            guard error == nil else{ // if error has value
+                completion(.error(.apiError)) // pass in api error
+                return
+            }
+            // if we have a response -  range 200 -299 success
+            guard let response = response as? HTTPURLResponse, 200..<300 ~= response.statusCode else{
+                completion(.error(.badResponse)) // if not pass in bad response
+                return
+            }
+            // decode data - try to decode it, pass value, pass data
+            guard let value = try? JSONDecoder().decode(V.self, from: data!) else{
+                completion(.error(.jsonDecoder)) // if fails, pass json decoder error
+                return
+            }
+            // call completion with value, pass in value
+            completion(.value(value))
+        }
+        task.resume() // in order to create the api request to the api
+    }
 }
--- WeatherApp/WeatherApp/API/Weather.swift
+++ WeatherApp/WeatherApp/API/Weather.swift
@@ -9,40 +9,47 @@
 // json api data example
 //http://api.wunderground.com/api/8eb0cec557955d5f/forecast10day/q/CA/San_Francisco.json
 
+// structure that models the weather response that we get from the api
 
 import Foundation
 
-class Weather: Codable {
-    let forcast: Forecast // first item in response
+// model the response using the Codable protocol
+
+// forcast is a dictionary in the response - model this first
+class Weather: Codable { // conform to Codable
+    let forcast: Forecast // root of response
 }
+
 // structures
+// forcast text
 struct Forecast: Codable {
     let forecastText: ForecastText
-    
+    // tells swift that response has different keys than provided in property names
     private enum CodingKeys: String, CodingKey {
-        case forecastText = "txt_forecast" // key name in response
+        case forecastText = "txt_forecast" // key name in response    case object = variable
     }
 }
 
+// 10 day forcast
 struct ForecastText: Codable {
-    let date: String
+    let date: String // date in response
     let forcastDays: [ForecastDay]  // array in response
-    
+    // tells swift that response has different keys than provided in property names
     private enum CodingKeys: String, CodingKey {
         case date // variable same as key
         case forcastDays = "forcastday"  // key name in response
     }
 }
 
+// get weather data
 struct ForecastDay: Codable{
     let iconUrl: String
     let day: String
     let description: String
-    
+    // tells swift that response has different keys than provided in property names
     private enum CodingKeys: String, CodingKey {
         case iconUrl = "icon_url" // key name in response
         case day = "title" // key name in response
         case description = "fcttext" // key name in response
-        
     }
 }
