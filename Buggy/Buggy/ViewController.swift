988f7b2152d9d496617b626429f3c2d84eaf7a8e
user135340 <user135340@NY166.local>
2018-02-12 07:25:28 -0500

    Buggy ch.9 complete

--- Buggy/Buggy/ViewController.swift
+++ Buggy/Buggy/ViewController.swift
@@ -21,10 +21,37 @@ class ViewController: UIViewController {
     }
     
     // implement an action method for the button to trigger
-    @IBAction func buttonTapped(_ sender: UIButton) {
-        // add a print() statement to the buttonTapped(_:) method to confirm that the method is called in response to a button tap.
-        print("Called buttonTapped(_:)")
+    //@IBAction func buttonTapped(_ sender: UIButton) {
+    //@IBAction func switchToggled(_ sender: UISwitch) {
+    //@IBAction func buttonTapped(_ sender: UISwitch) {
+    @IBAction func buttonTaped(_ sender: UIButton) {
+    
+    
+    // add a print() statement to the buttonTapped(_:) method to confirm that the method is called in response to a button tap.
+        //print("Called buttonTapped(_:)")
+        
+        //To illustrate the use of these literal expressions
+        print("Method: \(#function) in file: \(#file) line: \(#line) called.")
+        
+        badMethod()
+        
+        // Log sender:
+        //print("sender: \(sender)")
+        
+        // Log the control state:
+        //print("Is control on? \(sender.isOn)")
     }
-
+    
+    func badMethod() {
+        let array = NSMutableArray()
+        for i in 0..<10 {
+            array.insert(i, at: i)
+        }
+        // Go one step too far emptying the array (notice the range change):
+        for _ in 0..<10 {
+            //array.remove(at: 0)   // book error
+            array.remove(0)
+        }
+    }
 }
 
